51eadc73e686a9fe30bb7d113d38d33fe4809d1a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-11-26 08:37:42 -0500

    Increase verbosity

--- P3/bully/lib/bully.ex
+++ P3/bully/lib/bully.ex
@@ -12,6 +12,7 @@ defmodule Bully do
 
   defp connect() do
     # sup_node = Enum.at(@node_list, rem(rank - 1, length(@node_list)))
+    :logger.info("Waiting for node connections")
     alive_nodes = Enum.filter(@node_list, fn node -> Node.ping(node) == :pong end)
     if length(alive_nodes) == 0 do
       connect()
@@ -20,8 +21,10 @@ defmodule Bully do
 
   def start(rank) do
     Process.register(self(), :bully)
+    :logger.info("Node #{Node.self()} is starting with rank #{rank}")
     connect()
     # Announce rank to all available nodes
+    :logger.info("Announcing rank to other nodes")
     Enum.map(
       Node.list, fn n -> send({:bully, n}, {:rank, Node.self(), rank}) end)
     loop(Node.self(), %{Node.self() => rank})
@@ -32,13 +35,16 @@ defmodule Bully do
     max_rank = Map.get(rank_map, coordinator)
     receive do
       {:rank, node_name, node_rank} ->
+        :logger.info("Node #{node_name} has rank #{node_rank}")
         rank_map = Map.put(rank_map, node_name, node_rank)
         max_rank = max(rank, node_rank)
         if node_rank == rem(rank - 1, length(Node.list)) do
+          :logger.info("I shall monitor node #{node_name}")
           Node.monitor(node_name, :true)
         end
         # Declare victory
         coordinator = if rank == max_rank && Map.size(rank_map) == length(Node.list) do
+          :logger.info("I shall become coordinator")
           Enum.map(
             Node.list, fn n -> send({:bully, n}, {:victory, Node.self()}) end)
           Node.self()
@@ -47,11 +53,13 @@ defmodule Bully do
         end
         loop(coordinator, rank_map)
       {:victory, coordinator_node} ->
-        IO.puts "Coordinator now is #{coordinator_node}"
+        :logger.info("Coordinator now is #{coordinator_node}")
         loop(coordinator_node, rank_map)
       {:nodedown, node} ->
+        :logger.info("Node #{node} is down!")
         dead_rank = Map.get(rank_map, node)
         if dead_rank == max_rank do
+          :logger.info("Coordinator is dead!")
           # Coordinator is dead!
           candidates = Enum.filter(rank_map, fn {_, v} -> v > rank end)
                      |> Enum.into(%{})
@@ -60,13 +68,15 @@ defmodule Bully do
           receive do
             {:ok, _} -> loop(coordinator, rank_map)
             after
-              1_000 -> Enum.map(Node.list, fn n -> send({:bully, n}, {:victory, Node.self()}) end)
+              1_000 -> :logger.info("No one answered the election call, I shall become coordinator")
+                       Enum.map(Node.list, fn n -> send({:bully, n}, {:victory, Node.self()}) end)
                        loop(Node.self(), rank_map)
           end
 
         end
         loop(coordinator, rank_map)
       {:election, caller} ->
+        :logger.info("#{caller} has called for an election")
         send({:bully, caller}, {:ok, Node.self()})
         Enum.map(
           Node.list, fn n -> send({:bully, n}, {:rank, Node.self(), rank}) end)
